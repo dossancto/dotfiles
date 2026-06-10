2cd51393f3106e7a8922c55d9f10717a51ecc5be
dossancto <dossantossilvalucas963@gmail.com>
2026-06-10 20:05:46 -0300

    dont remap cd

--- fish/alias.fish
+++ fish/alias.fish
@@ -5,7 +5,7 @@ alias d 'docker'
 alias tf 'tofu'
 alias tb 'npx tb'
 alias clear "printf '\033[2J\033[3J\033[1;1H'"
-alias cd="z"
+# alias cd="z"
 alias rm="rip"
 alias k="kubectl"
 alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
