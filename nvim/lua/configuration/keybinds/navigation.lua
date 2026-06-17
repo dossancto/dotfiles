3f78dfa4324808da830af4ae4bbc661ec9f21044
dossancto <dossantossilvalucas963@gmail.com>
2026-06-17 13:44:22 -0300

    use yazi as explorer

--- nvim/lua/configuration/keybinds/navigation.lua
+++ nvim/lua/configuration/keybinds/navigation.lua
@@ -14,3 +14,7 @@ map('n', 'gvr', function()
 end, { desc = 'LSP: Go to References (vertical split)' })
 
 map('n', 'F', "<cmd> lua require('spectre').toggle() <cr>", { desc = 'Open Spectre' })
+
+map('n', '<F1>', "<cmd>Yazi<cr>", { desc = 'Open Explorer' })
+map('n', '<F13>', "<cmd>Yazi cwd<cr>", { desc = 'Open Explorer' })
+-- map('n', '<F25>', "<cmd>Yazi<cr>", { desc = 'Open Explorer' })
--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -1,3 +1,4 @@
+---@type LazySpec
 return {
 
 	{
@@ -68,5 +69,43 @@ return {
 			{ "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
 			{ "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
 		},
-	}
+	},
+	"mikavilpas/yazi.nvim",
+	version = "*",
+	event = "VeryLazy",
+	dependencies = {
+		{ "nvim-lua/plenary.nvim", lazy = true },
+	},
+	keys = {
+		{
+			"<leader>-",
+			mode = { "n", "v" },
+			"<cmd>Yazi<cr>",
+			desc = "Open yazi at the current file",
+		},
+		{
+			-- Open in the current working directory
+			"<leader>cw",
+			"<cmd>Yazi cwd<cr>",
+			desc = "Open the file manager in nvim's working directory",
+		},
+		{
+			"<c-up>",
+			"<cmd>Yazi toggle<cr>",
+			desc = "Resume the last yazi session",
+		},
+	},
+	---@type YaziConfig | {}
+	opts = {
+		open_for_directories = false,
+		keymaps = {
+			show_help = "<f1>",
+		},
+	},
+	init = function()
+		-- mark netrw as loaded so it's not loaded at all.
+		--
+		-- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+		vim.g.loaded_netrwPlugin = 1
+	end,
 }
